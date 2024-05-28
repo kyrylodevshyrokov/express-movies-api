6bae01363deecd5366f545574183d91eed630006
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-05-28 14:19:28 +0300

    added routes for movie and person (actor) search

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,43 @@
 var express = require("express");
+const movies = require("../data/movies");
+const people = require("../data/people");
 var router = express.Router();
 
+function queryRequired(req, res, next) {
+  const searchTerm = req.query.query;
+
+  if (!searchTerm) {
+    res.json({ msg: "Query is required" });
+  }
+  next();
+}
+
+router.use(queryRequired);
+
 /* GET search page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
+});
+
+router.get("/movie", (req, res, next) => {
+  const searchTerm = req.query.query;
+
+  const results = movies.filter((movie) => {
+    found =
+      movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
+});
+
+router.get("/person", (req, res, next) => {
+  const searchTerm = req.query.query;
+
+  const results = people.filter((person) => {
+    found = person.name.includes(searchTerm);
+    return found;
+  });
+  res.json({ results });
 });
 
 module.exports = router;
